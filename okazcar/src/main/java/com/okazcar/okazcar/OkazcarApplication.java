b93866095381ba11e079bde0be16a1c4682d81a2
Rakotomavo Manoa Harivony Ellias <114306980+HarivonyEllias@users.noreply.github.com>
2024-01-08 18:11:02 +0300

    Add files via upload

---
+++ okazcar/src/main/java/com/okazcar/okazcar/OkazcarApplication.java
@@ -0,0 +1,13 @@
+package com.okazcar.okazcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkazcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkazcarApplication.class, args);
+	}
+
+}
