3bc5f29842c661f79dbeee8c98274756f5b47985
JordieRayanne <119185845+JordieRayanne@users.noreply.github.com>
2024-01-09 23:59:26 +0300

    Delete okazcar/src/main/java/com/okazcar/okazcar/repository directory

--- okazcar/src/main/java/com/okazcar/okazcar/repository/MarqueRepository.java
+++
@@ -1,9 +0,0 @@
-package com.okazcar.okazcar.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.okazcar.okazcar.model.Marque;
-
-public interface MarqueRepository extends JpaRepository<Marque,Integer> {
-    
-}
