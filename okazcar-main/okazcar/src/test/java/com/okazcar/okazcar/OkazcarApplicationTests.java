29eb8cb698afac886360556a6a5e1f92a68d6398
IrinaKaren <121242769+IrinaKaren@users.noreply.github.com>
2024-01-10 00:45:19 +0300

    Add files via upload

---
+++ okazcar-main/okazcar/src/test/java/com/okazcar/okazcar/OkazcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okazcar.okazcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkazcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
