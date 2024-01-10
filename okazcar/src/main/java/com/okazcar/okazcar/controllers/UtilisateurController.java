08df22231892711660a13243ff2faadfaf4bb83b
mahery <mahery_fitahiana@yahoo.com>
2024-01-10 22:43:06 +0300

    Logout

--- okazcar/src/main/java/com/okazcar/okazcar/controllers/UtilisateurController.java
+++ okazcar/src/main/java/com/okazcar/okazcar/controllers/UtilisateurController.java
@@ -11,7 +11,6 @@ import com.okazcar.okazcar.services.UtilisateurService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
