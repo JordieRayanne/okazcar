faec7e8cb0be0e839284a6f7aceff89737612aea
Rakotomavo Manoa Harivony Ellias <114306980+HarivonyEllias@users.noreply.github.com>
2024-01-12 19:06:07 +0300

    Delete okazcar/src/main/java/com/okazcar/okazcar/repository directory

--- okazcar/src/main/java/com/okazcar/okazcar/repository/CategorieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.okazcar.okazcar.repository;
-
-import com.okazcar.okazcar.model.Categorie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategorieRepository extends JpaRepository<Categorie, Integer> {
-}
