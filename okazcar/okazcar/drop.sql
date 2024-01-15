f2766391b653bb3c8d70c818563d96e208d9becd
IrinaKaren <121242769+IrinaKaren@users.noreply.github.com>
2024-01-15 18:20:53 +0300

    Delete okazcar/okazcar directory

--- okazcar/okazcar/drop.sql
+++
@@ -1,5 +0,0 @@
-drop table client_voiture;
-drop table annonce;
-drop table commission;
-drop table voiture_utilisateur;
-drop table voiture;
--- okazcar/okazcar/src/main/java/com/okazcar/okazcar/repositories/V_Annonce_Repository.java
+++
@@ -1,9 +0,0 @@
-package com.okazcar.okazcar.repository;
-
-import com.okazcar.okazcar.model.V_Annonce;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface V_Annonce_Repository extends JpaRepository<V_Annonce, Integer> {
-}
