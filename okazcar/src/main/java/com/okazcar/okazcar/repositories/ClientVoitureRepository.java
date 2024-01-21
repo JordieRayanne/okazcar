1734f5ef650088e7b2c9695045d02ace269a5dbf
JordieRayanne <119185845+JordieRayanne@users.noreply.github.com>
2024-01-21 18:50:17 +0300

    Add files via upload
    client voiture repository

---
+++ okazcar/src/main/java/com/okazcar/okazcar/repositories/ClientVoitureRepository.java
@@ -0,0 +1,11 @@
+package com.okazcar.okazcar.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.okazcar.okazcar.models.ClientVoiture;
+
+@Repository
+public interface ClientVoitureRepository extends JpaRepository<ClientVoiture,Integer> {
+    
+}
