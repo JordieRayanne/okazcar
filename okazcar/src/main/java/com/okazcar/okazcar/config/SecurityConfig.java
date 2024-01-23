dccbe3be6fb50c2673d70c8857963c8ca9a1cb87
mahery <mahery_fitahiana@yahoo.com>
2024-01-23 20:14:34 +0300

    SecurityConfig

--- okazcar/src/main/java/com/okazcar/okazcar/config/SecurityConfig.java
+++ okazcar/src/main/java/com/okazcar/okazcar/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
 		http
 				.authenticationProvider(authenticationProvider())
 				.authorizeHttpRequests(auth -> auth
-						.requestMatchers("/utilisateur/**","/annonces/**")
+						.requestMatchers("/**")
 						.permitAll()
 						.anyRequest()
 						.authenticated())
