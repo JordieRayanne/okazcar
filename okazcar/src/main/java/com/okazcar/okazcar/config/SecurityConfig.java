9b04a09c6181889440ba213fa95739ceb52ef534
mahery <mahery_fitahiana@yahoo.com>
2024-02-08 16:49:44 +0300

    Notification opérationnel

--- okazcar/src/main/java/com/okazcar/okazcar/config/SecurityConfig.java
+++ okazcar/src/main/java/com/okazcar/okazcar/config/SecurityConfig.java
@@ -90,6 +90,7 @@ public class SecurityConfig {
 		config.addAllowedOrigin("http://localhost:3000");
 		config.addAllowedOrigin("http://localhost:8100");
 		config.addAllowedOrigin("http://localhost:3001");
+		config.addAllowedOrigin("https://localhost");
 		config.addAllowedOrigin("https://okazcar.netlify.app");
 		config.setAllowedMethods(Arrays.asList("POST", "OPTIONS", "GET", "DELETE", "PUT"));
 		config.setAllowedHeaders(Arrays.asList("*"));
@@ -105,7 +106,7 @@ public class SecurityConfig {
 			@Override
 			public void addCorsMappings( CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:3000", "http://localhost:8100", "http://localhost:3001", "https://okazcar.netlify.app")
+						.allowedOrigins("http://localhost:3000", "http://localhost:8100", "http://localhost:3001", "https://okazcar.netlify.app", "https://localhost")
 						.allowedMethods("GET", "POST", "PUT", "DELETE")
 						.allowedHeaders("*")
 						.allowCredentials(true);
